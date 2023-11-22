0e27ace35c73a5e2b2ccad94203f17baa1d1ee64
sangeon <sangeon6248@naver.com>
2023-11-23 00:58:59 +0900

    First Commit

---
+++ src/main/java/com/example/Club_Union/ClubUnionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Club_Union;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubUnionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubUnionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Club_Union/ClubUnionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Club_Union;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubUnionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
